727f223413d111ac1b66bd017e975c964d97d02c
keie <hombrehealth@hotmail..com>
2018-09-09 19:14:56 -0500

    changes layers for frameworks' layers

---
+++ ProyectoHospital/BusinessRules/BrConnection.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessRules
+{
+    using System;
+    using System.Web;
+    using System.Configuration;
+    
+
+    public class brConnection
+    {
+        public string ConnectionString { get; set; }
+
+        public string ConnectionString2 { get; set; }
+        string ArchiveLog { get; set; }
+        public brConnection()
+        {
+            ConnectionString = ConfigurationManager.ConnectionStrings["AZURE"].ConnectionString;
+            ArchiveLog = ConfigurationManager.AppSettings["MiPc"];
+        }
+
+       /* public void RecordLog(string ErrorMessage, string ErrorDetails)
+        {
+            beLog obeLog = new beLog();
+            obeLog.idUser = System.Security.Principal.WindowsIdentity.GetCurrent().Name;
+            obeLog.IpClient = HttpContext.Current.Request.UserHostAddress;
+            obeLog.Application = ConfigurationManager.AppSettings["Application"];
+            obeLog.dateTime = DateTime.Now;
+            obeLog.ErrorMessage = ErrorMessage;
+            obeLog.ErrorDetails = ErrorDetails;
+            brSaveLog.SaveLog(obeLog, ArchiveLog);
+        }*/
+    }
+}
---
+++ ProyectoHospital/Entity/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity
+{
+    public class Employee
+    {
+        public Int32 CodigoEmployee { get; set; }
+        public string LastName { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public string Account { get; set; }
+        public string Password { get; set; }
+        public string Turn { get; set; }
+    }
+}
